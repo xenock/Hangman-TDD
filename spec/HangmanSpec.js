e89d4b85f3c337b0be2c6568d8668b42d30e21f7
Xenock <miguelmoracho@gmail.com>
2017-04-14 17:22:54 +0200

    Letters into an array and getting words from dictionary

--- spec/HangmanSpec.js
+++ spec/HangmanSpec.js
@@ -4,4 +4,60 @@ describe('Hangman', function(){
   beforeEach(function() {
     hangman = new Hangman()
   })
+
+  describe('Attributes', function() {
+    describe('Dictionary', function() {
+      it('should have a dictionary as an array', function() {
+        expect(Array.isArray(hangman.dictionary)).toBe(true)
+      })
+      it('should not be empty', function() {
+        expect(hangman.dictionary.length > 0).toBe(true)
+      })
+    })
+    describe('letters', function(){
+      it('should be an array', function(){
+        expect(Array.isArray(hangman.letters)).toBe(true)
+      })
+      it('should be empty', function() {
+        expect(hangman.letters.length == 0).toBe(true)
+      })
+    })
+  })
+
+  describe('Functions', function() {
+    describe('getWord', function() {
+      it('should be a function', function() {
+        expect(typeof(hangman.getWord)).toBe('function')
+      })
+      it('should return a string', function() {
+        expect(typeof(hangman.getWord())).toBe('string')
+      })
+      it('should return a non empty string', function() {
+        expect(hangman.getWord().length > 0).toBe(true)
+      })
+      it('should return a random word', function() {
+        var word1 = hangman.getWord()
+        var word2 = hangman.getWord()
+        expect(word1 !== word2).toBe(true)
+      })
+    })
+    describe('randomNumber', function() {
+      it('should be a function', function() {
+        expect(typeof(hangman.randomNumber)).toBe('function')
+      })
+      it('should return a random number between 0 and dictionary.length', function() {
+        var l = hangman.dictionary.length
+        expect(hangman.randomNumber(l) != hangman.randomNumber(l)).toBe(true)
+      })
+    })
+    describe('pushLetters', function() {
+      it('should be a function', function() {
+        expect(typeof(hangman.pushLetters)).toBe('function')
+      })
+      it('should push letters into letters attribute', function() {
+        hangman.pushLetters('a')
+        expect(hangman.letters[0] === 'a').toBe(true)
+      })
+    })
+  })
 })
--- src/Hangman.js
+++ src/Hangman.js
@@ -1,3 +1,64 @@
 function Hangman() {
-  
+  this.dictionary = [
+    "Ant",
+    "APL",
+    "AutoHotkey scripting language",
+    "AutoIt scripting language",
+    "BASIC",
+    "DATABUS",
+    "DM",
+    "Eiffel",
+    "Forth",
+    "FPr",
+    "Game Maker Language",
+    "Groovy",
+    "Haskell",
+    "J",
+    "Julia",
+    "JavaScript",
+    "Lisp",
+    "Tea",
+    "LPC",
+    "Lua",
+    "MUMPS",
+    "Maple",
+    "Mathematica",
+    "MATLAB",
+    "Oriel",
+    "Pascal",
+    "PCASTL",
+    "Perl",
+    "PHP",
+    "Pikt",
+    "PostScript",
+    "PROSE",
+    "Python",
+    "REXX",
+    "R",
+    "Ruby",
+    "S-Lang",
+    "Standard ML",
+    "Spin",
+    "Tcl",
+    "TI-BASIC",
+    "TorqueScript",
+    "thinBasic scripting language",
+    "VBScript",
+    "Windows PowerShell",
+    "Wolfram Language",
+    "Some scripting languages",
+  ]
+  this.letters = []
+}
+
+Hangman.prototype.getWord = function() {
+  return this.dictionary[this.randomNumber(this.dictionary.length)]
+}
+
+Hangman.prototype.randomNumber = function(max) {
+  return Math.floor(Math.random() * max)
+}
+
+Hangman.prototype.pushLetters = function(letter){
+  return this.letters.push(letter)
 }
